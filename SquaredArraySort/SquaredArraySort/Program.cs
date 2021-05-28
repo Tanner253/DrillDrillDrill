ede4577ada20d68ed7beec2f76f6a478913aadee
Tanner Percival <percivaltanner@gmail.com>
2021-05-28 12:20:24 -0700

    adds solution for sorted squared array

---
+++ SquaredArraySort/SquaredArraySort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SquaredArraySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, -10 };
+            int[] collection = SortedSquaredArray(array);
+            foreach(var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+            
+                }
+        public static int[] SortedSquaredArray(int[] array)
+        {
+            int[] output = new int[array.Length];
+            for(var i = 0; i < array.Length; i++)
+            {
+                output[i] = array[i] * array[i];
+            }
+            Array.Sort(output);
+            return output;
+        }
+    }
+}
