b52291b0f270352972ed2e901aa848c44ef1f20a
Tanner Percival <percivaltanner@gmail.com>
2021-05-29 10:43:39 -0700

    adds solution for node depths

---
+++ NodeDepths/NodeDepths/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NodeDepths
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int NodeDepths(BinaryTree root)
+        {
+            return NodeDepthsHelper(root, 0);
+        }
+
+        public static int NodeDepthsHelper(BinaryTree root, int depth)
+        {
+            if (root == null) return 0;
+            return depth + NodeDepthsHelper(root.lChild, depth + 1) + NodeDepthsHelper(root.rChild, depth + 1);
+
+        }
+
+        public class BinaryTree
+        {
+            public int value;
+            BinaryTree lChild;
+            BinaryTree rChild;
+            public BinaryTree(int value)
+            {
+                this.value = value;
+                lChild = null;
+                rChild = null;
+            }
+        }
+    }
+}
