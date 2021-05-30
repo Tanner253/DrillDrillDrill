ae8ebdd588b4364c3de60cc7a48da0f0311d54eb
Tanner Percival <percivaltanner@gmail.com>
2021-05-30 15:57:17 -0700

    adds solution to Caesar cypher

--- CaesarCypherEncryptor/CaesarCypherEncryptor/Program.cs
+++ CaesarCypherEncryptor/CaesarCypherEncryptor/Program.cs
@@ -7,6 +7,26 @@ namespace CaesarCypherEncryptor
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //this problem is an exercise that is great for understaninding modulus
+        }
+
+        public static string CaesarCypherEncryptor(string str, int key)
+        {
+            char[] newLetters = new char[str.Length];
+            //allows us to remove duplicate passes, as each 26 go by we are doing 1 loop effectivly duplicating work.
+            int newKey = key % 26;
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                newLetters[i] = GetNewLetter(str[i], key, alphabet);
+            }
+            return new string(newLetters);
+        }
+        public static char GetNewLetter(char letter, int key, string alphabet)
+        {
+            int newLetterCode = alphabet.IndexOf(letter) + key;
+            return alphabet[newLetterCode % 26];
         }
     }
 }
