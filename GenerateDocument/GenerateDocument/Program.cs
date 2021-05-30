5dd862f2383bf249f02caa929cb84b663922f723
Tanner Percival <percivaltanner@gmail.com>
2021-05-30 16:51:35 -0700

    adds solution for GenerateDocument

---
+++ GenerateDocument/GenerateDocument/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenerateDocument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static bool GenerateDocument(string characters, string document)
+        {
+            HashSet<char> alreadyCounted = new HashSet<char>();
+
+            for(int i = 0; i < document.Length; i++)
+            {
+                char character = document[i];
+                if (alreadyCounted.Contains(character))
+                {
+                    continue;
+                }
+                int documentFrequency = GetCharFrequency(character, document);
+                int charactersFrequency = GetCharFrequency(character, characters);
+
+                if(documentFrequency > charactersFrequency)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static int GetCharFrequency(char character, string target)
+        {
+            int frequency = 0;
+            for(int i = 0; i < target.Length; i++)
+            {
+                char c = target[i];
+                if( c == character)
+                {
+                    frequency += 1;
+                }
+            }
+            return frequency;
+        }
+    }
+}
